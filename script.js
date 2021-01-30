89e02c0fd226c9e6cd2085f25ba4a57bd9f8e8ed
Paulo Ferreira <57817565+PauloFerreira2000@users.noreply.github.com>
2021-01-30 14:32:22 -0300

    corrigindo para quebrar quando tecla Enter

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 function gerar() {
   res.innerHTML = "Comando:"
-
+  
   var m = document.getElementById('matri').value //pega a matricula
-  var teste = m.split(" ") //separa cada matricula por espaço
+  var newStringM = m.replace(/\n/g," "); //retira todos Enter, espaço a mais da matricula (não tira espaço TAB)
+
+  var teste = newStringM.split(" ") //separa cada matricula por espaço
   var filtered_matri = teste.filter(function (el) { //limpa os arrays que estão vazios
     return el != "";
   });
+  
+  
+  var f = document.getElementById('funci').value //pega o cod funcionario
+  var newStringF = f.replace(/\n/g," "); //retira todos Enter, espaço a mais da matricula (não tira espaço TAB)
 
-  var f = document.getElementById('funci').value //pega a matricula
-  var teste = f.split(" ") //separa cada matricula por espaço
+  var teste = newStringF.split(" ") //separa cada cod por espaço
   var filtered_funci = teste.filter(function (el) { //limpa os arrays que estão vazios
     return el != "";
   });
